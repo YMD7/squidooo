3bcb7aa2b2b27cbc705bd95d979d36cc9c8cc0f1
YMD7 <hysteric.madam@gmail.com>
2017-08-03 16:01:37 +0900

    [update]当選結果の確認を追加

--- app/services/login_services/tokyo_ota_city_service.rb
+++ app/services/login_services/tokyo_ota_city_service.rb
@@ -5,12 +5,13 @@ module LoginServices
       @browser = service.browser
       login_to(service.dest)
     end
-  
+
     private
-    
+
     def login_to(dest)
-      if dest == "register_lottery"     then n = 1 end
-      if dest == "comfirm_registration" then n = 2 end
+      n = 1 if dest == "register_lottery"
+      n = 2 if dest == "comfirm_registrations"
+      n = 3 if dest == "comfirm_results"
       @browser.element(xpath: "/html/body/form/table/tbody/tr/td[1]/div[4]/table/tbody/tr/td[2]/table/tbody/tr[#{n}]/td[2]/a").click
       @browser.text_field( xpath: '/html/body/form[2]/div/div[2]/table/tbody/tr/td/div/table[2]/tbody/tr/td[1]/input[1]' ).set cerfs[:login_id]
       @browser.text_field( xpath: '/html/body/form[2]/div/div[2]/table/tbody/tr/td/div/table[2]/tbody/tr/td[1]/input[2]' ).set cerfs[:password]
