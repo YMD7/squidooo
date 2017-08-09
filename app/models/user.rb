ac66c05e591f0abb0281d1b29cfbf135a2673699
Kyo Yamada <hysteric.madam@gmail.com>
2017-08-09 22:31:11 +0900

    [add]Installing devise to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :service_accounts
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
