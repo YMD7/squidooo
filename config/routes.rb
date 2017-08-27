33910df3d9eab242b2579495a4928bc0611511d4
Kyo Yamada <hysteric.madam@gmail.com>
2017-08-28 00:08:42 +0900

    [add]ログインページのコーディングスタート

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
+
+  root 'pages#login'
+
   get 'pages/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
