ed4811951ae121d47c276d227a82cf49bae53830
Kyo Yamada <hysteric.madam@gmail.com>
2017-10-29 19:12:23 +0900

    add render(), can show the component

--- frontend/javascripts/app.js
+++ frontend/javascripts/app.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // ---------------------------------------
 //                          + components +
 // ---------------------------------------
 import {
   Hello
 } from "../components";
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById("contents")
+);
