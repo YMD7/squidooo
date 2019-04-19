c81a77af0673f0330549d8de6a96c7180a88e83c
Kyo Yamada <hysteric.madam@gmail.com>
2019-04-19 21:11:31 +0900

    finish styling SNS button

--- components/SigninButton.vue
+++ components/SigninButton.vue
@@ -2,10 +2,10 @@
 button
   .icon
     SNSIcon(:sns="sns")
-  .text
+  .text(:class="sns")
     | {{ sns }}
     span.small
-      |  でログイン
+      | &nbsp;でログイン
 </template>
 
 <script>
@@ -25,8 +25,36 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+button
+  display flex
+  background-color #FFFFFF
+  border 6px solid #FFFFFF
+  border-radius 6px
+  box-shadow 5px 7px #000000
+  width 242px
+  height 65px
+
+.icon
+  display flex
+  justify-content center
+  align-items center
+  width 42px
+  height 44px
+
 .text
+  display flex
+  justify-content center
+  align-items center
+  padding-block-start 1px
+  width 175px
+  height 44px
   font-size 0.95rem
+  &.Google
+    color googleColor
+  &.Facebook
+    color facebookColor
+  &.Twitter
+    color twitterColor
 
 .small
   font-size 0.88rem
